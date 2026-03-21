6137b4c8c91aef699fe7f562cc4ba7a5edeff295
nikivavlt <nikivavlt@gmail.com>
2026-03-21 12:50:22 +0200

    Remove endpoint for testing

--- main.go
+++ main.go
@@ -13,9 +13,6 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ws", h.ServeWS)
-	mux.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("ok"))
-	})
 
 	log.Println("Game server listening on :8080")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", mux))
